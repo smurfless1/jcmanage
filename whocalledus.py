0ff618c632edd63aa6262e97c2e14b0aa2911ede
Dave Brown <david.brown@nominum.com>
2014-04-30 11:33:42 -0700

    Score checking programs and libs

---
+++ whocalledus.py
@@ -0,0 +1,89 @@
+# Library/Program whocalledus. Get a score for a phone number from whocalled.us
+# Copyright (C) 2014 David Brown
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import urllib
+import urllib2
+
+from ratelimited import RateLimited
+
+
+class whocalled:
+    """
+    See http://whocalled.us/about
+    Abstraction interface to simplify getting scores from whocalled.us website
+    via their API.
+
+    THIS REQUIRES YOU TO HAVE AN ACCOUNT. An account must be made on their site to use their API.
+
+    http://whocalled.us/do?action=
+    do?action=report&name=test&pass=test&phoneNumber=5551234567&date=2007-03-06&callerID=800%20Service&identity=TeleCon&postalCode=90210
+    do?action=comment&name=test&pass=test&phoneNumber=5551234567&message=Test
+    do?action=getScore&name=test&pass=test&phoneNumber=5551234567
+    """
+
+    def __init__(self, name, passwd):
+        self.site = 'http://whocalled.us/do'
+        self.name = name
+        self.passwd = passwd
+        self.headers = {'User-Agent': 'Mozilla/4.0 (compatible; MSIE 5.5; Windows NT)'}
+
+    def _sendit(self, values):
+        url = self.site
+        data = urllib.urlencode(values)
+        req = urllib2.Request(url, data, self.headers)
+        response = urllib2.urlopen(req)
+        return response.read()
+
+    @RateLimited(1)
+    def getScore(self, number):
+        values = {
+                'action':'getScore',
+                'name' : self.name,
+                'pass': self.passwd,
+                'phoneNumber' : number
+        }
+        try:
+            response = self._sendit(values)
+            success = response.split('&')[0].split('=')[1]
+            if success == "1":
+                score = response.split('&')[1].split('=')[1]
+                return score
+        except:
+            pass
+        return "Failed to get a response."
+
+    @RateLimited(1)
+    def report(self, number, dateString='', callerString='', guessedName='',
+        zipcode=''):
+        values = {
+            'action': 'report',
+            'name': self.name,
+            'pass': self.passwd,
+            'phoneNumber': number,
+            'date': dateString,
+            'callerID': callerString,
+            'identity': guessedName,
+            'postalCode': zipcode
+        }
+        response = self._sendit(values)
+        #TODO not sure if I care about results.
+
+if __name__ == '__main__':
+    import sys
+    raise NotImplementedError  # when you put in your password, take this out
+    wc = whocalled('YourUserName', 'YourPassword')
+    score = wc.getScore(sys.argv[1])
+    print score
+
---
+++ whocallsme.py
@@ -0,0 +1,72 @@
+# Library/program whocallsme.py. Get a score from whocallsme.com
+# Copyright (C) 2014 David Brown
+#
+# This program is free software: you can redistribute it and/or modify it under
+# the terms of the GNU General Public License as published by the Free Software
+# Foundation, either version 3 of the License, or (at your option) any later
+# version.
+#
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of  MERCHANTABILITY or FITNESS
+# FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License along with
+# this program.  If not, see <http://www.gnu.org/licenses/>.
+
+import time
+
+from selenium import webdriver
+from selenium.common.exceptions import NoSuchElementException
+
+
+class whocallsme:
+    """
+    This tries to grade a caller based on information found on WhoCallsMe.com
+    """
+
+    def __init__(self):
+        self.browser = None
+        self.site = 'http://whocallsme.com/nb/search.aspx?q='
+        self.site2 = 'http://whocallsme.com/Phone-Number.aspx/'
+
+    def getScore(self, number):
+        returnme = "0"
+        try:
+            # chrome doesn't quit?
+            #if os.path.exists("/Applications/Google Chrome.app"):
+            #    self.browser = webdriver.Chrome()
+            # os.path.exists("/Applications/Firefox.app"):
+            #self.browser = webdriver.Firefox()
+            #else:
+            #    self.browser = webdriver.Safari()
+            self.browser = webdriver.Firefox()
+
+            #self.browser.get( self.site  + number)
+            self.browser.get( self.site2  + number)
+
+            try:
+                # use this to get numbers from front page
+                # phones = self.browser.find_elements_by_css_selector('a.oos_previewTitle')
+                # detail page, count of comments:
+                returnme = \
+                    str(len(self.browser.find_elements_by_css_selector('li.oos_postListItem')))
+                #returnme = self.browser.find_elements_by_id('resInfo-1')[0].text.split()[1]
+
+            except NoSuchElementException:
+                assert 0, "Can't find listings"
+        except:
+            print "Couldn't get a proper response."
+        time.sleep(5)
+        self.browser.close()
+        self.browser.quit()
+        return returnme
+
+    def report(self, number, dateString='', callerString='', guessedName='',
+        zipcode=''):
+        pass
+
+if __name__ == '__main__':
+    wc = whocallsme()
+    score = wc.getScore('01233648537')
+    print score
+
